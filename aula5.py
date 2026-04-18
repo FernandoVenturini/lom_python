023519aa27b1deeb8583ec79faf0b2774a6f906d
Fernando <fernandoventurini9@hotmail.com>
2026-04-18 19:31:53 +0100

    bool, int, float #05

---
+++ aula5.py
@@ -0,0 +1,13 @@
+# Tipo bool (bolean): true ou false
+# Operadores de comparação: ==, !=, >, <, >=, <=
+# Operadores lógicos: and, or, not
+
+# Exemplo de comparação de idade:
+idade = 18
+print(idade >= 18)  # True, pois 18 é maior ou igual a 18
+
+# Exemplo de comparação de strings:
+nome1 = "Alice"
+nome2 = "Bob"
+print(nome1 == nome2)  # False, pois "Alice" é diferente de "Bob"
+print(type(nome1 == nome2))  # <class 'bool'>, o resultado é do tipo booleano
