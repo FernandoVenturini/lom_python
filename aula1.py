5098c4a8808899d331a9f1163c93720ed4386c05
Fernando <fernandoventurini9@hotmail.com>
2026-04-18 10:32:11 +0100

    starting project lom_python #01 #02

---
+++ aula1.py
@@ -0,0 +1,17 @@
+# Permite escrever um comentario
+print(123) # Imprime o numero 123
+print("Olá, mundo!") # Imprime a string "Olá, mundo!" 
+
+# Comentarios podem ser usados para explicar o código, ou para deixar notas para outros programadores (ou para você mesmo no futuro). Eles são ignorados pelo interpretador Python, então não afetam a execução do programa.
+''' Comentarios de múltiplas linhas podem ser feitos usando três aspas simples ou duplas. Eles são úteis para explicar blocos de código ou para deixar notas mais detalhadas. '''
+
+# Exemplo de comentario de múltiplas linhas usando aspas duplas
+"""
+Este é um comentário de múltiplas linhas.
+Ele pode ser usado para explicar um bloco de código ou para deixar notas mais detalhadas.
+"""
+
+# Exemplo de comentario de múltiplas linhas usando aspas simples
+''' Este é outro exemplo de comentário de múltiplas linhas. 
+Ele também pode ser usado para explicar um bloco de código ou para deixar notas mais detalhadas.
+'''
---
+++ aula2.py
@@ -0,0 +1,9 @@
+# Funcao print
+print("Hello World") # Imprime a string "Hello World" na tela
+
+print(12, 34, sep="-", end="\r\n") # Imprime os numeros 12 e 34 na tela, separados por um traço
+print(56, 78, sep='-', end="\n") # Imprime os numeros 56 e 78 na tela, separados por um traço
+print(9, 10, sep='-', end="\n") # Imprime os numeros 9 e 10 na tela, separados por um traço
+
+
+
