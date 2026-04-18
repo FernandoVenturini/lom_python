f22e5294047469da47d81d256d25417151a917ad
Fernando <fernandoventurini9@hotmail.com>
2026-04-18 11:00:56 +0100

    type int, float #04

---
+++ aula4.py
@@ -0,0 +1,15 @@
+# Tipo int e float
+# int: números inteiros
+# float: números decimais
+
+# Exemplo de int
+idade = 25
+print("Idade:", idade)
+
+# Exemplo de float
+altura = 1.75
+print("Altura:", altura)
+
+print(type('Fernando'))  # Saída: <class 'str'>
+print(type(idade))  # Saída: <class 'int'>
+print(type(altura))  # Saída: <class 'float'>
