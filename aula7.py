28736b64accc8384a8b3cdca84aa2032e7b4cf2b
Fernando <fernandoventurini9@hotmail.com>
2026-04-20 20:51:28 +0100

    data type #07

--- aula7.py
+++ aula7.py
@@ -3,6 +3,7 @@ nome = "Fernando" # string
 sobrenome = "Venturini" # string
 idade = 45 # inteiro
 altura = 1.80 # float
+
 # Imprimindo as variaveis
 print("Nome:", nome)
 print("Sobrenome:", sobrenome)
---
+++ aula8.py
@@ -0,0 +1,16 @@
+# Tipo de dados: 
+nome = "Fernando"
+sobrenome = "Venturini"
+idade = 45.7
+ano_de_nascimento = 2026 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.86
+
+print(
+    "Nome:", nome, 
+    "Sobrenome:", sobrenome, 
+    "Idade:", idade, 
+    "Ano de nascimento:", ano_de_nascimento, 
+    "Maior de idade:", maior_de_idade, 
+    "Altura em metros:", altura_metros
+    )
