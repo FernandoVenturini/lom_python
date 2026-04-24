d676a79d2afed04587623047dd081eeab79d73d9
Fernando <fernandoventurini9@hotmail.com>
2026-04-24 09:07:07 +0100

    arithmetic operators #27

---
+++ aula5.py
@@ -0,0 +1,30 @@
+# INTRODUCAO AOS OPERADORES ARITMETICOS
+# Operadores Aritmeticos: +, -, *, /, //, **, %
+
+# Soma
+adicao = 10 + 10
+print("Adição: ", adicao)
+
+# Subtração
+subtracao = 10 - 5
+print("Subtração: ", subtracao)
+
+# Multiplicação
+multiplicacao = 10 * 10
+print("Multiplicação: ", multiplicacao)
+
+# Divisão
+divisao = 10 / 3
+print("Divisão: ", divisao)
+
+# Divisão Inteira
+divisao_inteira = 10 // 3
+print("Divisão Inteira: ", divisao_inteira)
+
+# Potência
+potencia = 10 ** 5
+print("Potência: ", potencia)
+
+# Módulo
+modulo = 55 % 2 # O operador de módulo retorna o resto da divisão entre dois números. Neste caso, 10 dividido por 5 é igual a 2, e o resto é 0, portanto o resultado é 0.
+print("Módulo: ", modulo)
