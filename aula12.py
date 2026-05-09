ba8ed5f661fb352aba798f90a4196d499cbefdf8
Fernando <fernandoventurini9@hotmail.com>
2026-05-09 10:41:13 +0100

    String Formatting #32

---
+++ aula12.py
@@ -0,0 +1,9 @@
+# INTRODUCAO AS f-strings (formatacao de string)
+
+nome = "Fernando"
+altura = 1.86
+peso = 83
+imc = peso / (altura ** 2)
+
+# f-strings
+print(f"Meu nome e  {nome} e meu imc e {imc:.2f}!") # :.2f -> formata o numero para 2 casas decimais
