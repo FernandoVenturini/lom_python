a95905efaf705b583a8a4e74187dc799295e3099
Fernando <fernandoventurini9@hotmail.com>
2026-05-09 10:32:55 +0100

    BMI Calculation #31

---
+++ aula11.py
@@ -0,0 +1,9 @@
+# EXERCICIO DE PROGRAMACAO - AULA 11 - CALCULO IMC + ELLIPSIS
+
+# CALCULO IMC
+nome = 'Fernando'
+altura = 1.86
+peso = 83
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura e pesa', peso, 'kg. O IMC é', round(imc, 2)) 
