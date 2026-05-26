66e7586158a282c4883c5f4f7aee6f1549c2d7b5
Fernando <fernandoventurini9@hotmail.com>
2026-05-26 20:36:52 +0100

    if/elif/else

---
+++ aula36.py
@@ -0,0 +1,12 @@
+# if / elif / else
+
+condicao = 1
+condicao2 = 2
+condicao3 = 3
+
+if condicao:
+    print('Este e o codigo do if!')
+elif condicao2:
+    print('Condicao 2')
+else:
+    print('Condicao 3')
