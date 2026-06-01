ccfe8e0fbb985f7db0d625ef141198016c3f148b
Fernando <fernandoventurini9@hotmail.com>
2026-06-01 20:27:16 +0100

    debugger

---
+++ aula17.py
@@ -0,0 +1,15 @@
+# if / elif / else
+
+condicao1 = True
+condicao2 = False
+condicao3 = False
+condicao4 = True
+
+if condicao1:
+    print('Este e o codigo do if! Condicao 1!')
+elif condicao2:
+    print('Este e o codigo do if! Condicao 2!')
+elif condicao3:
+    print('Este e o codigo do if! Condicao 3!')
+else:
+    print("Este e o codigo do if! Condicao 4!")
