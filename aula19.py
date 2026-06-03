4f08253ca5129231467d76f4e8d76cbf8fb02f64
Fernando <fernandoventurini9@hotmail.com>
2026-06-03 23:00:07 +0100

    adding operators

---
+++ aula19.py
@@ -0,0 +1,18 @@
+# OPERADORES DE COMPARACAO:
+
+# == (igual a)
+# != (diferente de)
+# > (maior que)
+# < (menor que)
+# >= (maior ou igual a)
+# <= (menor ou igual a)
+
+# Exemplo:
+a = 10
+b = 20
+print(a == b)  # False
+print(a != b)  # True
+print(a > b)   # False
+print(a < b)   # True
+print(a >= b)  # False
+print(a <= b)  # True
